715280c6c6cba35a36815181429604e4ee423d36
Melissa Dias <107875967+DiasMelissa@users.noreply.github.com>
2025-10-03 11:36:09 -0300

    Update printVariable.py

--- scripts/printVariable.py
+++ scripts/printVariable.py
@@ -1,9 +1,9 @@
 from netCDF4 import Dataset
 from wrf import getvar
 
-ncfile = Dataset("wrfout_d01_2016-10-07_00_00_00")
+ncfile = Dataset("wrfout_d01_2018-02-18_00_00_00")
 
-# Get the Sea Level Pressure (slp) for others variables see https://wrf-python.readthedocs.io/en/latest/diagnostics.html
+# Get the Sea Level Pressure (slp). See more variables: https://wrf-python.readthedocs.io/en/latest/diagnostics.html
 slp = getvar(ncfile, "slp")
 
 print(slp)
